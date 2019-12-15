bc93414919e0bad6d8b1c1b9518bc79d460f62e8
RednedEpic <redned235@gmail.com>
2019-12-15 15:45:20 -0600

    Make Location cloneable

--- modules/api/src/main/java/org/battleplugins/world/Location.java
+++ modules/api/src/main/java/org/battleplugins/world/Location.java
@@ -10,75 +10,80 @@ import lombok.RequiredArgsConstructor;
 @Getter
 @AllArgsConstructor
 @RequiredArgsConstructor
-public class Location {
+public class Location implements Cloneable {
 
-	/**
-	 * The world the location is in.
-	 *
-	 * @return the world the location is in
-	 *
-	 */
-	private final World world;
+    /**
+     * The world the location is in.
+     *
+     * @return the world the location is in
+     *
+     */
+    private final World world;
 
-	/**
-	 * The x coordinate of the location
-	 *
-	 * @return the x coordinate of the location
-	 */
-	private final double x;
+    /**
+     * The x coordinate of the location
+     *
+     * @return the x coordinate of the location
+     */
+    private final double x;
 
-	/**
-	 * The y coordinate of the location
-	 *
-	 * @return the y coordinate of the location
-	 */
-	private final double y;
+    /**
+     * The y coordinate of the location
+     *
+     * @return the y coordinate of the location
+     */
+    private final double y;
 
-	/**
-	 * The z coordinate of the location
-	 *
-	 * @return the z coordinate of the location
-	 */
-	private final double z;
+    /**
+     * The z coordinate of the location
+     *
+     * @return the z coordinate of the location
+     */
+    private final double z;
 
-	/**
-	 * The pitch of the location
-	 *
-	 * @return the pitch of the location
-	 */
-	private float pitch;
+    /**
+     * The pitch of the location
+     *
+     * @return the pitch of the location
+     */
+    private float pitch;
 
-	/**
-	 * The yaw of the location
-	 *
-	 * @return the yaw of the location
-	 */
-	private float yaw;
+    /**
+     * The yaw of the location
+     *
+     * @return the yaw of the location
+     */
+    private float yaw;
 
-	/**
-	 * The block x coordinate of the location
-	 *
-	 * @return the block x coordinate of the location
-	 */
-	public int getBlockX() {
-		return (int) Math.floor(x);
-	}
+    /**
+     * The block x coordinate of the location
+     *
+     * @return the block x coordinate of the location
+     */
+    public int getBlockX() {
+        return (int) Math.floor(x);
+    }
 
-	/**
-	 * The block y coordinate of the location
-	 *
-	 * @return the block y coordinate of the location
-	 */
-	public int getBlockY() {
-		return (int) Math.floor(y);
-	}
+    /**
+     * The block y coordinate of the location
+     *
+     * @return the block y coordinate of the location
+     */
+    public int getBlockY() {
+        return (int) Math.floor(y);
+    }
 
-	/**
-	 * The block z coordinate of the location
-	 *
-	 * @return the block z coordinate of the location
-	 */
-	public int getBlockZ() {
-		return (int) Math.floor(z);
-	}
+    /**
+     * The block z coordinate of the location
+     *
+     * @return the block z coordinate of the location
+     */
+    public int getBlockZ() {
+        return (int) Math.floor(z);
+    }
+
+    @Override
+    public Location clone() {
+        return new Location(world, x, y, z, pitch, yaw);
+    }
 }
