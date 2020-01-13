0891f1562d9b36ec41c5f7edab9f88a1368b20f8
RednedEpic <redned235@gmail.com>
2020-01-12 21:01:12 -0600

    Add a getLocation method in BlockEntity

--- modules/api/src/main/java/org/battleplugins/api/world/block/entity/BlockEntity.java
+++ modules/api/src/main/java/org/battleplugins/api/world/block/entity/BlockEntity.java
@@ -1,11 +1,21 @@
 package org.battleplugins.api.world.block.entity;
 
+import org.battleplugins.api.world.Location;
 import org.battleplugins.api.world.block.Block;
 
 /**
  * Represents a block entity.
  */
 public interface BlockEntity {
+
+    /**
+     * The {@link Location} of the block entity
+     *
+     * @return the location of the block entity
+     */
+    default Location getLocation() {
+        return getBlock().getLocation();
+    }
 
     /**
      * The {@link Block} that corresponds to this block
