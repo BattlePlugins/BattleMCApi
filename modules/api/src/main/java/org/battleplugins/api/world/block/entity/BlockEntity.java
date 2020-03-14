1dd62a6ace0b4793e859f0d21d3299a0bc7f16c1
RednedEpic <redned235@gmail.com>
2020-03-13 19:48:23 -0500

    Add a method to easily get the world in BlockEntity

--- modules/api/src/main/java/org/battleplugins/api/world/block/entity/BlockEntity.java
+++ modules/api/src/main/java/org/battleplugins/api/world/block/entity/BlockEntity.java
@@ -1,12 +1,22 @@
 package org.battleplugins.api.world.block.entity;
 
 import org.battleplugins.api.world.Location;
+import org.battleplugins.api.world.World;
 import org.battleplugins.api.world.block.Block;
 
 /**
  * Represents a block entity.
  */
 public interface BlockEntity {
+
+    /**
+     * The {@link World} this block entity is located in
+     *
+     * @return the world this block entity is located in
+     */
+    default World getWorld() {
+        return getLocation().getWorld();
+    }
 
     /**
      * The {@link Location} of the block entity
